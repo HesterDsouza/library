732a85e650d1ce4c9c7de823cb96905a6147da5a
Hester D'Souza <hester.dsouza.08@gmail.com>
2024-08-09 12:22:26 +0530

    Updated js and CSS

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ document.getElementById('bookForm').addEventListener('submit', (event) => {
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
-    const read = document.getElementById('read').value;
+    const read = document.getElementById('read').checked;
 
     const newBook = new Book(title, author, pages, read);
     addBookToLibrary(newBook);
